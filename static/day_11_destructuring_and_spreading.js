c8cfc954639edb8e5329b0f2e6c979ae74e1f9c9
Nguyen N Bao Tram <tramnguyenngocbao2000@gmail.com>
2024-04-10 00:30:38 +0700

    comlete exercises day 11

--- static/day_11_destructuring_and_spreading.js
+++ static/day_11_destructuring_and_spreading.js
@@ -56,7 +56,7 @@ const fullStack = [
 for (const [first, second, third] of fullStack) {
   console.log(first, second, third);
 }
-*/
+
 const rectangle = {
   width: 20,
   height: 10,
@@ -227,3 +227,173 @@ const sumAllNums = (...args) => {
   return sum;
 };
 console.log(sumAllNums(1, 2, 3, 4, 5));
+*/
+/*
+const constants = [2.72, 3.14, 9.81, 37, 100];
+let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants;
+
+const countriess = ["Finland", "Estonia", "Sweden", "Denmark", "Norway"];
+let [fin, est, sw, den, nor] = countriess;
+
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200,
+  perimeter: 60,
+};
+let { width, height, area, perimeter } = rectangle;
+
+const users = [
+  {
+    name: "Brook",
+    scores: 75,
+    skills: ["HTM", "CSS", "JS"],
+    age: 16,
+  },
+  {
+    name: "Alex",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "David",
+    scores: 75,
+    skills: ["HTM", "CSS"],
+    age: 22,
+  },
+  {
+    name: "John",
+    scores: 85,
+    skills: ["HTML"],
+    age: 25,
+  },
+  {
+    name: "Sara",
+    scores: 95,
+    skills: ["HTM", "CSS", "JS"],
+    age: 26,
+  },
+  {
+    name: "Martha",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "Thomas",
+    scores: 90,
+    skills: ["HTM", "CSS", "JS"],
+    age: 20,
+  },
+];
+// get the keys with destructuring
+for (const { name, scores, skills, age } of users) {
+  console.log(Object.keys({ name, scores, skills, age }));
+}
+// get the keys without destructuring
+for (const element of users) {
+  console.log(Object.keys(element));
+}
+// get the values with destructuring
+for (const { name, scores, skills, age } of users) {
+  console.log(Object.values({ name, scores, skills, age }));
+}
+// Find the people who have less than two skills
+for (const { name, scores, skills, age } of users) {
+  if (skills.length < 2) {
+    console.log(name);
+  }
+}
+*/
+// Destructure the countries object, then print name, capital, population and languages of all countries
+/*
+ import { countries } from "./countries_data.js";
+const countriesArr = countries;
+for (const { name, capital, languages, population, ...rest } of countriesArr) {
+  console.log(name, capital, population, languages);
+}
+*/
+// A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+/*
+const student = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
+let [name, skills, [, , jsScore, reactScore]] = student;
+console.log(name, skills, jsScore, reactScore);
+*/
+// 3. Write a function called convertArrayToObject which can convert the array to a structure object.
+/*
+const students = [
+  ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]],
+  ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
+];
+const convertArrayToObject = (arr) => {
+  const newArray = [];
+  for (const [name, skills, scores] of arr) {
+    newArray.push({ name: name, skills: skills, scores: scores });
+  }
+  return newArray;
+};
+console.log(convertArrayToObject(students))
+*/
+// 4. Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+/*const student = {
+  name: "David",
+  age: 25,
+  skills: {
+    frontEnd: [
+      { skill: "HTML", level: 10 },
+      { skill: "CSS", level: 8 },
+      { skill: "JS", level: 8 },
+      { skill: "React", level: 9 },
+    ],
+    backEnd: [
+      { skill: "Node", level: 7 },
+      { skill: "GraphQL", level: 8 },
+    ],
+    dataBase: [{ skill: "MongoDB", level: 7.5 }],
+    dataScience: ["Python", "R", "D3.js"],
+  },
+};
+*/
+// this approach has been changed the original object student,
+// but the quiz asked me not to mutate the original
+/*
+const newStudent = { ...student };
+const { name: n, age: a, skills: s } = newStudent;
+const { frontEnd, backEnd, dataBase, dataScience } = s;
+frontEnd.push({ skill: "Bootstrap", level: 8 });
+backEnd.push({ skill: "Express", level: 9 });
+dataBase.push({ skill: "SQL", level: 8 });
+dataScience.push("SQL");
+console.log(newStudent);
+console.log(student);
+*/
+/*
+const newStudent = Object.assign({}, student);
+const { name, age, skills } = newStudent;
+const { frontEnd, backEnd, dataBase, dataScience } = skills;
+console.log(student);
+console.log(frontEnd, backEnd, dataBase, dataScience);
+skills.frontEnd.push({ skill: "Bootstrap", level: 8 });
+skills.backEnd = { skill: "Express", level: 9 };
+skills.dataBase = { skill: "SQL", level: 8 };
+skills.dataScience = ["SQL"];
+console.log(name, age, skills);
+console.log(
+  newStudent.skills.frontEnd.length === student.skills.frontEnd.length
+);
+*/
+/* this approach is not to mutate the original student
+const newStudent = JSON.parse(JSON.stringify(student));
+const { name, age, skills } = newStudent;
+const { frontEnd, backEnd, dataBase, dataScience } = skills;
+console.log(student);
+frontEnd.push({ skill: "Bootstrap", level: 8 });
+backEnd.push({ skill: "Express", level: 9 });
+dataBase.push({ skill: "SQL", level: 8 });
+dataScience.push("SQL");
+console.log(name, age, skills);
+console.log(
+  newStudent.skills.frontEnd.length === student.skills.frontEnd.length
+);
+*/
